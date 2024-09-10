864bb7cb06e797056da02705c138890ed046613a
GuiAlbqrqe <guialbqrqe@outlook.com>
2024-09-10 11:33:48 -0300

    Implementing new data in the customer register

--- Cadastro.py
+++ Cadastro.py
@@ -2,33 +2,54 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
-def gravar_dados(nome, data_nasc, tipo):
+def gravar_dados(nome, cep, endereco, numero_casa, ponto_referencia, email, data_nasc, tipo):
     if nome and data_nasc <= date.today():
         with open("clientes.csv", "a", encoding="utf-8") as file:
-            file.write(f"{nome},{data_nasc},{tipo}\n")
+            file.write(f'"{nome}";"{cep}";"{endereco}";"{numero_casa}";"{ponto_referencia}";"{email}";"{data_nasc}";"{tipo}"\n')
         st.session_state["sucesso"] = True
     else:
         st.session_state["sucesso"] = False
 
 st.set_page_config(
     page_title="Cadastro de clientes",
-    page_icon="📙"
+    page_icon="📙",
+    layout="wide"
 )
 
 st.title("Cadastro de clientes")
+col1, col2 = st.columns(2)
 st.divider()
 
-nome = st.text_input("Digite o nome do cliente",
+with col1:
+    nome = st.text_input("Digite o Nome do cliente",
                      key="nome_cliente")
 
-dt_nasc = st.date_input("Data nascimento", min_value=date(1900, 1, 1), max_value=date.today(), format="DD/MM/YYYY")
+    endereco = st.text_input("Digite o Endereço do Cliente",
+                         key="endereco_cliente")
+    
+    dt_nasc = st.date_input("Data Nascimento", min_value=date(1900, 1, 1), max_value=date.today(), format="DD/MM/YYYY")
 
-tipo = st.selectbox("Tipo do cliente",
-                    ["Pessoa jurídida", "Pessoa física"])
+
+    email = st.text_input("Digite o Email do Cliente",
+                        key="email_cliente")
+with col2:
+
+    cep = st.text_input("Digite o CEP do Cliente",
+                    key="cep_cliente")
+
+
+    numero_casa = st.text_input("N° do Endereço e Tipo do Endereço",
+                            key="numero_casa")
+
+    ponto_referencia = st.text_input("Ponto de Referência",
+                            key="pronto_referencia")
+
+    tipo = st.selectbox("Tipo do cliente",
+                    ["Pessoa Jurídida", "Pessoa Física"])
 
 btn_cadastrar = st.button("Cadastrar", 
                           on_click=gravar_dados,
-                          args=[nome, dt_nasc, tipo])
+                          args=[nome, cep, endereco, numero_casa, ponto_referencia, email, dt_nasc, tipo])
 
 if btn_cadastrar:
     if st.session_state["sucesso"]:
@@ -37,4 +58,3 @@ if btn_cadastrar:
     else:
         st.error("Houve algum proglema no cadastro!",
                 icon="❌")
-
--- pages/Consulta.py
+++ pages/Consulta.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-dados = pd.read_csv("clientes.csv")
+dados = pd.read_csv("clientes.csv", sep=";", header=0,dtype={"CEP": str})
 
-st.title("Clientes cadastrados")
+dados['Data de Nascimento'] = pd.to_datetime(dados['Data de Nascimento']).dt.strftime('%d/%m/%Y')
+
+num_linhas = len(dados)
+altura = min(400 + (num_linhas * 35), 800)
+
+st.title("Clientes Cadastrados")
 st.divider()
 
-st.dataframe(dados)
+st.dataframe(dados, height=altura)
