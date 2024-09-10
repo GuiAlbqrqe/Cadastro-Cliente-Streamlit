a7c21100b393bbddc5008b818dd7be95da4a518b
Guilherme <guialbqrqe@outlook.com>
2024-09-10 16:52:59 -0300

    Implementing changes and adding information for a complete registration

--- Cadastro.py
+++ Cadastro.py
@@ -21,31 +21,31 @@ col1, col2 = st.columns(2)
 st.divider()
 
 with col1:
-    nome = st.text_input("Digite o Nome do cliente",
+    nome = st.text_input("Nome do cliente",placeholder="Digite o nome do Cliente",
                      key="nome_cliente")
 
-    endereco = st.text_input("Digite o Endereço do Cliente",
+    endereco = st.text_input("Endereço do Cliente",placeholder="Digite o Endereço do Cliente",
                          key="endereco_cliente")
     
-    dt_nasc = st.date_input("Data Nascimento", min_value=date(1900, 1, 1), max_value=date.today(), format="DD/MM/YYYY")
+    dt_nasc = st.date_input("Data Nascimento", value=None,min_value=date(1900, 1, 1), max_value=date.today(), format="DD/MM/YYYY")
 
 
-    email = st.text_input("Digite o Email do Cliente",
+    email = st.text_input("E-mail do Cliente",placeholder="Digite o E-mail do Cliente",
                         key="email_cliente")
 with col2:
 
-    cep = st.text_input("Digite o CEP do Cliente",
+    cep = st.text_input("CEP do Cliente",placeholder="Digite o CEP do Cliente",
                     key="cep_cliente")
 
 
-    numero_casa = st.text_input("N° do Endereço e Tipo do Endereço",
+    numero_casa = st.text_input("N° e Tipo",placeholder="Digite o Número e Tipo do Endereço",
                             key="numero_casa")
 
-    ponto_referencia = st.text_input("Ponto de Referência",
+    ponto_referencia = st.text_input("Ponto de Referência",placeholder="Digite um Ponto de Referência",
                             key="pronto_referencia")
 
-    tipo = st.selectbox("Tipo do cliente",
-                    ["Pessoa Física","Pessoa Jurídica"])
+    tipo = st.selectbox("Tipo do Cliente",
+                    ("Pessoa Jurídica", "Pessoa Física"), index=None, placeholder="Pessoa Física ou Jurídica?", key="tipo_cliente")
 
 btn_cadastrar = st.button("Cadastrar", 
                           on_click=gravar_dados,
@@ -55,6 +55,7 @@ if btn_cadastrar:
     if st.session_state["sucesso"]:
         st.success("Cliente cadastrado com sucesso!",
                    icon="✅")
+        
     else:
         st.error("Houve algum proglema no cadastro!",
                 icon="❌")
